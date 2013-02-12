9d03aea2466bb0ca0abaa72acd11b8709cea3c1f
tokkidaggers <olli.kortelahti@pp.inet.fi>
2013-02-12 13:24:43 +0200

    ebin

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
 $(document).ready(function() {
 	var cnv = document.getElementById('main');
 	var ctx = cnv.getContext('2d');
-	ctx.fillRect(20,20,60,60);
-	var posx = 20;
-	var posy = 20;
+	ctx.fillRect(0,0,60,60);
+	var posx = 0;
+	var posy = 0;
 	loaded();
-	$(document).on('keypress', function(event) {
-			console.log('ebin');
-		if (event.which == 40 ) {
-			event.preventDefault();
-			ctx.clearRect(0,0,500,500);
-		}
+	$(document).on('keydown', function(event) {
+			if (event.which == 40 ) {
+				event.preventDefault();
+				ctx.clearRect(0, 0, 500, 500);
+				ctx.fillRect(posx, posy+60, 60, 60);
+				posy = posy+60;
+			}
+			if (event.which == 39) {
+				event.preventDefault();
+				ctx.clearRect(0, 0, 500, 500);
+				ctx.fillRect(posx+60, posy, 60, 60);
+				posx = posx+60;
+			}
+			if (event.which ==  37) {
+				event.preventDefault();
+				ctx.clearRect(0, 0, 500, 500);
+				ctx.fillRect(posx-60, posy, 60, 60);
+				posx = posx-60;
+			}
+			if (event.which == 38) {
+				event.preventDefault();
+				ctx.clearRect(0, 0, 500, 500);
+				ctx.fillRect(posx, posy-60, 60, 60);
+				posy = posy-60;
+			}
 	});
+
 });
+
 
 function loaded() {
 	$('div#load').show();
